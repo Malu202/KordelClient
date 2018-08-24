2a833a9d9abb6cae8331ff2d5349331baf4c6792
Malu202 <poehlmann.martin@gmail.com>
2018-08-24 21:15:23 +0200

    implement snackbar notifications

--- src/scripts/tools.js
+++ src/scripts/tools.js
@@ -1,89 +1,93 @@
 //TOOLS
 var getRequest = function (url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
-    xhr.addEventListener('load', function (event) {
-      var json = JSON.parse(xhr.responseText);
-      update(json);
-      callback(json);
-    });
-    xhr.send();
-  }
-  
-  var postRequest = function (url, jsondata, callback) {
-    var http = new XMLHttpRequest();
-    http.open("POST", url, true);
-  
-    http.setRequestHeader("Content-type", "application/json");
-  
-    http.onreadystatechange = function () {
-      if (http.readyState == 4) {
-        var responseJSON = JSON.parse(http.responseText);
-        update(responseJSON);
-        callback(responseJSON);
-      }
+  var xhr = new XMLHttpRequest();
+  xhr.open("GET", url, true);
+  xhr.addEventListener('load', function (event) {
+    var json = JSON.parse(xhr.responseText);
+    update(json);
+    callback(json);
+  });
+  xhr.send();
+}
+
+var postRequest = function (url, jsondata, callback) {
+  var http = new XMLHttpRequest();
+  http.open("POST", url, true);
+
+  http.setRequestHeader("Content-type", "application/json");
+
+  http.onreadystatechange = function () {
+    if (http.readyState == 4) {
+      var responseJSON = JSON.parse(http.responseText);
+      const dataObj = {
+        message: responseJSON,
+      };
+      snackbar.show(dataObj);
+      update(responseJSON);
+      callback(responseJSON);
     }
-    http.send(JSON.stringify(jsondata));
   }
-  
-  var deleteRequest = function (url, jsondata, callback) {
-    var http = new XMLHttpRequest();
-    http.open("DELETE", url, true);
-  
-    http.setRequestHeader("Content-type", "application/json");
-  
-    http.onreadystatechange = function () {
-      if (http.readyState == 4) {
-        var responseJSON = JSON.parse(http.responseText);
-        update(responseJSON);
-        callback(responseJSON);
-      }
+  http.send(JSON.stringify(jsondata));
+}
+
+var deleteRequest = function (url, jsondata, callback) {
+  var http = new XMLHttpRequest();
+  http.open("DELETE", url, true);
+
+  http.setRequestHeader("Content-type", "application/json");
+
+  http.onreadystatechange = function () {
+    if (http.readyState == 4) {
+      var responseJSON = JSON.parse(http.responseText);
+      update(responseJSON);
+      callback(responseJSON);
     }
-    http.send(JSON.stringify(jsondata));
   }
-  var content = document.getElementById("content");
-  function showDialog(heading, body, cancel, accept, oncancel, onaccept) {
-    var dialog = document.createElement("aside");
-    dialog.className = "mdc-dialog";
-    dialog.role = "alertdialog";
-    var surface = document.createElement("div");
-    surface.className = "mdc-dialog__surface";
-    var header = document.createElement("header");
-    header.className = "mdc-dialog__header";
-    var h2 = document.createElement("h2");
-    h2.className = "mdc-dialog__header__title";
-    var headertext = document.createTextNode(heading);
-    var section = document.createElement("section");
-    section.className = "mdc-dialog__body";
-    var bodytext = document.createTextNode(body);
-    var footer = document.createElement("footer");
-    footer.className = "mdc-dialog__footer";
-    var buttonCancel = document.createElement("button");
-    buttonCancel.className = "mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--cancel";
-    buttonCancel.type = "button";
-    var buttonAccept = document.createElement("button");
-    buttonAccept.className = "mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--accept";
-    buttonAccept.type = "button";
-    var backdrop = document.createElement("div");
-    backdrop.className = "mdc-dialog__backdrop";
-    var canceltext = document.createTextNode(cancel);
-    var accepttext = document.createTextNode(accept);
-  
-    content.appendChild(dialog);
-    dialog.appendChild(surface);
-    surface.appendChild(header);
-    header.appendChild(h2);
-    h2.appendChild(headertext);
-    surface.appendChild(section);
-    section.appendChild(bodytext);
-    surface.appendChild(footer);
-    if (cancel != null) footer.appendChild(buttonCancel);
-    if (accept != null) footer.appendChild(buttonAccept);
-    buttonCancel.appendChild(canceltext);
-    buttonAccept.appendChild(accepttext);
-    dialog.appendChild(backdrop);
-    var dialogjs = new window.mdc.dialog.MDCDialog(dialog);
-    dialogjs.listen('MDCDialog:accept', onaccept);
-    dialogjs.listen('MDCDialog:cancel', oncancel);
-    dialogjs.show();
-  }
+  http.send(JSON.stringify(jsondata));
+}
+var content = document.getElementById("content");
+function showDialog(heading, body, cancel, accept, oncancel, onaccept) {
+  var dialog = document.createElement("aside");
+  dialog.className = "mdc-dialog";
+  dialog.role = "alertdialog";
+  var surface = document.createElement("div");
+  surface.className = "mdc-dialog__surface";
+  var header = document.createElement("header");
+  header.className = "mdc-dialog__header";
+  var h2 = document.createElement("h2");
+  h2.className = "mdc-dialog__header__title";
+  var headertext = document.createTextNode(heading);
+  var section = document.createElement("section");
+  section.className = "mdc-dialog__body";
+  var bodytext = document.createTextNode(body);
+  var footer = document.createElement("footer");
+  footer.className = "mdc-dialog__footer";
+  var buttonCancel = document.createElement("button");
+  buttonCancel.className = "mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--cancel";
+  buttonCancel.type = "button";
+  var buttonAccept = document.createElement("button");
+  buttonAccept.className = "mdc-button mdc-dialog__footer__button mdc-dialog__footer__button--accept";
+  buttonAccept.type = "button";
+  var backdrop = document.createElement("div");
+  backdrop.className = "mdc-dialog__backdrop";
+  var canceltext = document.createTextNode(cancel);
+  var accepttext = document.createTextNode(accept);
+
+  content.appendChild(dialog);
+  dialog.appendChild(surface);
+  surface.appendChild(header);
+  header.appendChild(h2);
+  h2.appendChild(headertext);
+  surface.appendChild(section);
+  section.appendChild(bodytext);
+  surface.appendChild(footer);
+  if (cancel != null) footer.appendChild(buttonCancel);
+  if (accept != null) footer.appendChild(buttonAccept);
+  buttonCancel.appendChild(canceltext);
+  buttonAccept.appendChild(accepttext);
+  dialog.appendChild(backdrop);
+  var dialogjs = new window.mdc.dialog.MDCDialog(dialog);
+  dialogjs.listen('MDCDialog:accept', onaccept);
+  dialogjs.listen('MDCDialog:cancel', oncancel);
+  dialogjs.show();
+}
