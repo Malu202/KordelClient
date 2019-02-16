e2cf913105e2054ecab111e83d131f715b6aeb3a
Malu202 <poehlmann.martin@gmail.com>
2019-02-16 21:12:19 +0100

    fix layout and only show episodes of selected season

--- src/pages/Serien.js
+++ src/pages/Serien.js
@@ -1,5 +1,5 @@
 var SERIEN_URL = serverip + "LokaleSerien";
-var SERIEN_URL = "http://kordel.selfhost.at/LokaleSerien.json" //MOCKUP FÜR DEBUGGING OHNE RASPBERRY, NICHT COMMITEN!!!!
+//var SERIEN_URL = "http://kordel.selfhost.at/LokaleSerien.json" //MOCKUP FÜR DEBUGGING OHNE RASPBERRY, NICHT COMMITEN!!!!
 getRequest(SERIEN_URL, function (res) {
     if (res.message == null) console.log("nix");
     else {
@@ -11,10 +11,10 @@ getRequest(SERIEN_URL, function (res) {
 serienPage = document.getElementById("serien");
 
 var serienBluePrint = document.getElementsByClassName("serienBluePrint")[0];
-var serienName = document.getElementsByClassName("serienName")[0];
+// var serienName = document.getElementsByClassName("serienName")[0];
 var serienStaffelOption = document.getElementsByClassName("serienStaffelOption")[0];
-var serienFolgeOption = document.getElementsByClassName("serienFolgeOption")[0];
-var folgeAbspielenButton = document.getElementsByClassName("folgeAbspielenButton")[0];
+// var serienFolgeOption = document.getElementsByClassName("serienFolgeOption")[0];
+// var folgeAbspielenButton = document.getElementsByClassName("folgeAbspielenButton")[0];
 
 function newSerienCard(serie) {
     var name = serie.name;
@@ -29,26 +29,44 @@ function newSerienCard(serie) {
     staffelSelect.innerHTML = "";
     var folgeSelect = neueSerie.getElementsByClassName("serienFolgeOption")[0].parentElement;
     folgeSelect.innerHTML = "";
+    var folgeAbspielenButton = neueSerie.getElementsByClassName("folgeAbspielenButton")[0];
 
     for (var i = 0; i < staffeln.length; i++) {
         var neueStaffel = neueOption.cloneNode(true);
         neueStaffel.innerHTML = staffeln[i].name;
-        neueStaffel.value = staffeln[i].name;
+        //neueStaffel.value = staffeln[i].name;
+        neueStaffel.value = i;
         staffelSelect.appendChild(neueStaffel);
-
+    }
+    
+    staffelOnChange = function(){        
+        var i = staffelSelect.value;
+        var staffeln = serie.staffeln;
+        var neueOption = serienStaffelOption.cloneNode(true);
+        folgeSelect.innerHTML = "";
         for (var j = 0; j < staffeln[i].folgen.length; j++) {
             var neueFolge = neueOption.cloneNode(true);
             neueFolge.innerHTML = staffeln[i].folgen[j].name;
-            neueFolge.value = staffeln[i].folgen[j].name;
+            //neueFolge.value = staffeln[i].folgen[j].name;
+            neueFolge.value = j;
             folgeSelect.appendChild(neueFolge);
         }
-    }
+    };
+    staffelSelect.addEventListener("change", staffelOnChange);
+
+    folgeAbspielenButton.addEventListener("click",function(){
+        var i = staffelSelect.value;
+        var j = folgeSelect.value;
+        requestSeriesPlayback(serie.staffeln[i].folgen[j].path);
+    })
+
     serienPage.appendChild(neueSerie);
+    staffelOnChange();    
 }
 
-function setupSerienCards(serien){
+function setupSerienCards(serien) {
     serienPage.innerHTML = "";
-    for(var i = 0; i<serien.length; i++){
+    for (var i = 0; i < serien.length; i++) {
         newSerienCard(serien[i]);
     }
 }
