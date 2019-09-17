525798e17c2be4f69b94405400714aa4127e3dbf
Malu202 <poehlmann.martin@gmail.com>
2019-09-18 00:44:25 +0200

    fix double use of player function names

--- src/pages/Football.js
+++ src/pages/Football.js
@@ -8,9 +8,9 @@ function updateFootball(response) {
     footballControls.classList = "footballcontrols";
 
     // footballControls.appendChild(fbButton("play", play));
-    footballControls.appendChild(fbButton("play/pause", pause));
-    footballControls.appendChild(fbButton("vorspulen", vorspulen));
-    footballControls.appendChild(fbButton("stop", stop));
+    footballControls.appendChild(fbButton("play/pause", pauseFootball));
+    footballControls.appendChild(fbButton("vorspulen", vorspulenFootball));
+    footballControls.appendChild(fbButton("stop", stopFootball));
 
 
     footballPage.appendChild(footballControls);
@@ -33,22 +33,22 @@ function updateFootball(response) {
     }
 }
 
-function vorspulen() {
+function vorspulenFootball() {
     var request = {};
     request.task = "fastForwardFootballGame";
     postRequest(serverip + "todo", request, function (msg) { });
 }
-function pause() {
+function pauseFootball() {
     var request = {};
     request.task = "pauseFootballGame";
     postRequest(serverip + "todo", request, function (msg) { });
 }
-function play() {
+function playFootball() {
     var request = {};
     request.task = "playFootballGame";
     postRequest(serverip + "todo", request, function (msg) { });
 }
-function stop() {
+function stopFootball() {
     var request = {};
     request.task = "stopFootballGame";
     postRequest(serverip + "todo", request, function (msg) { });
